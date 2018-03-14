296c7843da669cbd1c6b699454910bd10ec130f2
davidpodhola <david@podhola.net>
2018-03-14 21:35:19 +0100

    Customers WIP

--- src/kotlin/software/hsharp/business/services/ICustomers.kt
+++ src/kotlin/software/hsharp/business/services/ICustomers.kt
@@ -5,6 +5,6 @@ import software.hsharp.business.models.ICategory
 
 interface ICustomers {
     fun getAllCustomers() : Array<ICustomer>
-    fun getCustomerById( id : Int ) : ICustomer
+    fun getCustomerById( id : Int ) : ICustomer?
     fun getCustomersByAnyCategory( categories : Array<ICategory> ) : Array<ICustomer>
 }
